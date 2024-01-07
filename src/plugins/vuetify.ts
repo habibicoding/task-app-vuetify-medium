9e30f6cb11e5df486beea33ce0ff8c3ca9b5dc2e
Habibi Coding <habibicoding@aol.com>
2024-01-07 19:11:47 +0100

    chore: Change primary colors.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,9 +9,18 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
-import { createVuetify } from 'vuetify'
+import {createVuetify} from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  //
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#01793E', // green
+          secondary: '#CE1227', // red
+        },
+      },
+    },
+  },
 })
