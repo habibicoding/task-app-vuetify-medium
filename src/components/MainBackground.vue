f865ce68cb3dbeb0a66858c319b493ff98262994
Habibi Coding <habibicoding@aol.com>
2024-01-10 00:50:39 +0100

    chore: Add component for main app background.

---
+++ src/components/MainBackground.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app id="inspire">
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg" class="v-sheet-padding">
+              <slot></slot>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
