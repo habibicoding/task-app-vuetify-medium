c481bf7aeb2de1db762cc7ed84e5bf96c3c641a2
Habibi Coding <habibicoding@aol.com>
2024-01-10 00:36:01 +0100

    chore: Add constants.

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -8,3 +8,19 @@ export const HTTP_STATUS = {
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
 };
+
+export const OPEN_TASKS = 'Open Tasks';
+
+export const CLOSED_TASKS = 'Closed Tasks';
+
+export const ALL_TASKS = 'All Tasks';
+
+export const HOME_VIEW = 'Home';
+
+export const TASK_DETAIL_VIEW = 'TaskDetail';
+
+export const TASK_CREATE_VIEW = 'TaskCreate';
+
+export const TASK_UPDATE_VIEW = 'TaskUpdate';
+
+export const MIN_TASK_DESCRIPTION = 3;
