9b6898e8798689636842db82131673e93ee6990a
Habibi Coding <habibicoding@aol.com>
2024-01-10 01:30:36 +0100

    chore: Add a component for loading spinner when request gets send.

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  isLoading: {
+    type: Boolean,
+    required: true
+  }
+});
+
+</script>
+
+<template>
+  <div class="loading-container" v-if="isLoading">
+    <v-progress-circular indeterminate color="primary"></v-progress-circular>
+  </div>
+</template>
+
+<style scoped>
+
+.loading-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+
+</style>
