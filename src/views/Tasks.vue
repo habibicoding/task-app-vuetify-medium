ba83a54b71f619c5f30c009b1207f551c53a51d2
Habibi Coding <habibicoding@aol.com>
2024-01-10 02:37:39 +0100

    chore: Add first view.

---
+++ src/views/Tasks.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import {onMounted, ref, watch, watchEffect} from "vue";
+import router from "@/router";
+import {getTasks} from "@/composables/getTasks";
+import {TASK_DETAIL_VIEW, TASK_UPDATE_VIEW} from "@/constants/appConstants";
+import {TaskFetchResponse} from "@/dtos/taskDto";
+import {useTaskNavigation} from "@/composables/useTaskNavigation";
+import {useTaskStore} from "@/store/taskStore";
+
+
+const {fetchTasks, tasks, isLoading, isNetworkError, axiosError} = getTasks();
+const {handleTaskTypeSelected, logoClicked} = useTaskNavigation();
+const taskStore = useTaskStore();
+const selectedTaskId = ref(0);
+const isDeleteDialogSelected = ref(false);
+const selectedTaskDescription = ref('');
+
+
+onMounted(() => {
+  fetchTasks(taskStore.selectedTaskType);
+});
+
+watch(() => taskStore.selectedTaskType, (newType) => {
+  fetchTasks(newType);
+});
+
+watchEffect(() => {
+  fetchTasks(taskStore.selectedTaskType);
+});
+
+
+const openDeleteDialog = (task: { id: number, description: string }) => {
+  selectedTaskId.value = task.id;
+  selectedTaskDescription.value = task.description;
+  isDeleteDialogSelected.value = true;
+};
+
+const handleCardClicked = (id: number) => {
+  router.push({name: TASK_DETAIL_VIEW, params: {id: id.toString()}}).then();
+};
+
+const navigateToTaskUpdateView = (task: TaskFetchResponse) => {
+  taskStore.setTaskToEdit(task);
+  router.push({name: TASK_UPDATE_VIEW, params: {id: task.id.toString()}}).then();
+};
+
+const deleteTask = (id: number) => {
+  console.log("delete clicked");
+};
+
+</script>
+
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
