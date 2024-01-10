160fc6a2af3f04803a3f756e867033d39d53e2be
Habibi Coding <habibicoding@aol.com>
2024-01-11 00:35:02 +0100

    chore: Remove a file and adapt the default view.

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,9 @@
 <template>
   <v-app>
-    <DefaultBar />
-
     <DefaultView />
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  import DefaultBar from './default/AppBar.vue'
   import DefaultView from './default/View.vue'
 </script>
