48994581396de2c5d18137856239ba18f269cf33
Habibi Coding <habibicoding@aol.com>
2024-01-13 15:47:09 +0100

    chore: Add routing for updating a task.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 // Composables
 import {createRouter, createWebHistory} from 'vue-router'
-import {HOME_VIEW, TASK_CREATE_VIEW, TASK_DETAIL_VIEW} from "@/constants/appConstants";
+import {HOME_VIEW, TASK_CREATE_VIEW, TASK_DETAIL_VIEW, TASK_UPDATE_VIEW} from "@/constants/appConstants";
 import TaskCreatePage from "@/pages/TaskCreatePage.vue";
 import TasksOverviewPage from "@/pages/TasksOverviewPage.vue";
 import TaskDetailsPage from "@/pages/TaskDetailsPage.vue";
+import TaskUpdatePage from "@/pages/TaskUpdatePage.vue";
 
 
 const routes = [
@@ -29,6 +30,12 @@ const routes = [
         component: TaskDetailsPage,
         props: true,
       },
+      {
+        path: 'edit/:id',
+        name: TASK_UPDATE_VIEW,
+        component: TaskUpdatePage,
+        props: true,
+      }
     ],
   }
 ]
